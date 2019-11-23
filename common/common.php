109f7fea722f317d2a14a2e36534ac8c35a69bf8
budougumi0617 <budougumi0617@gmail.com>
2019-11-23 09:50:18 +0900

    :sparkles: P275 8-1 add CSV download page

--- common/common.php
+++ common/common.php
@@ -1,9 +1,10 @@
 <?php
 declare(strict_types=1);
 
-function sanitize(array $before): array{
+function sanitize(array $before): array
+{
     $after = array();
-    foreach ($before as $k => $v){
+    foreach ($before as $k => $v) {
         $after[$k] = htmlspecialchars($v, ENT_QUOTES, 'UTF-8');
     }
     return $after;
@@ -26,3 +27,67 @@ function convertToGengo(int $seireki): string
     return $gengo;
 }
 
+function pulldown_year()
+{
+    echo '<select name="year">';
+    echo '<option value="2017">2017</option>';
+    echo '<option value="2018">2018</option>';
+    echo '<option value="2019">2019</option>';
+    echo '<option value="2020">2020</option>';
+    echo '</select>';
+}
+
+function pulldown_month()
+{
+    echo '<select name="month">';
+    echo '<option value="01">01</option>';
+    echo '<option value="02">02</option>';
+    echo '<option value="03">03</option>';
+    echo '<option value="04">04</option>';
+    echo '<option value="05">05</option>';
+    echo '<option value="06">06</option>';
+    echo '<option value="07">07</option>';
+    echo '<option value="08">08</option>';
+    echo '<option value="09">09</option>';
+    echo '<option value="10">10</option>';
+    echo '<option value="11">11</option>';
+    echo '<option value="12">12</option>';
+    echo '</select>';
+}
+
+function pulldown_day()
+{
+    echo '<select name="month">';
+    echo '<option value="01">01</option>';
+    echo '<option value="02">02</option>';
+    echo '<option value="03">03</option>';
+    echo '<option value="04">04</option>';
+    echo '<option value="05">05</option>';
+    echo '<option value="06">06</option>';
+    echo '<option value="07">07</option>';
+    echo '<option value="08">08</option>';
+    echo '<option value="09">09</option>';
+    echo '<option value="10">10</option>';
+    echo '<option value="11">11</option>';
+    echo '<option value="12">12</option>';
+    echo '<option value="13">13</option>';
+    echo '<option value="14">14</option>';
+    echo '<option value="15">15</option>';
+    echo '<option value="16">16</option>';
+    echo '<option value="17">17</option>';
+    echo '<option value="18">18</option>';
+    echo '<option value="19">19</option>';
+    echo '<option value="20">20</option>';
+    echo '<option value="21">21</option>';
+    echo '<option value="22">22</option>';
+    echo '<option value="23">23</option>';
+    echo '<option value="24">24</option>';
+    echo '<option value="25">25</option>';
+    echo '<option value="26">26</option>';
+    echo '<option value="27">27</option>';
+    echo '<option value="28">28</option>';
+    echo '<option value="29">29</option>';
+    echo '<option value="30">30</option>';
+    echo '<option value="31">31</option>';
+    echo '</select>';
+}
--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -16,6 +16,8 @@ sessionCheck();
 <a href="../staff/staff_list.php">スタッフ管理</a><br/>
 <a href="../product/pro_list.php">商品管理</a>
 <br/>
+<a href="../order/order_download.php">注文ダウンロード</a><br/>
+<br/>
 <a href="../staff/staff_logout.php">ログアウト</a>
 </body>
 </html>
